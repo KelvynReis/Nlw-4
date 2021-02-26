e6e75bddb42e8fd0d719f0a2e0d89bfe606a21bc
kelvyn <kelvyn2reis@gmail.com>
2021-02-26 15:59:09 -0300

    create hook Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,49 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown(){
+
+    const [time, setTime] = useState(25 * 60);
+    const [active, setActive] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
+
+    function startCountdown(){
+        setActive(true);
+    }
+
+    useEffect(()=>{
+        if(active && time > 0){
+            setTimeout(() => {
+                setTime(time - 1);
+            }, 1000);
+        }
+        
+    }, [active, time])
+
     return(
         <div>
             <div className={styles.countdownContainer}>
             <div>
-                <span>2</span>
-                <span>5</span>
+                <span>{minuteLeft}</span>
+                <span>{minuteRight}</span>
             </div>
             <span>:</span>
             <div>
-                <span>0</span>
-                <span>0</span>
+                <span>{secondsLeft}</span>
+                <span>{secondsRight}</span>
             </div>
             </div>
 
-            <button type="button" className={styles.CountdownButton}>
+            <button
+                type="button"
+                className={styles.CountdownButton}
+                onClick={startCountdown}
+                >
                 Iniciar um ciclo
             </button>
         </div>
