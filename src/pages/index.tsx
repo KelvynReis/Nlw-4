b109a4fc27cba36785966ac06652475692f4ad7e
kelvyn <kelvyn2reis@gmail.com>
2021-02-26 15:36:57 -0300

    create components Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from 'next/head'
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
+
       <ExperienceBar/>
 
       <section>
         <div>
           <Profile/>
           <CompletedChallenges/>
+          <Countdown/>
         </div>
         <div>
 
