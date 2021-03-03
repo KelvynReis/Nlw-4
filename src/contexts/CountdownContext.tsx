8e6d8071b888ec4de61d48a0977c8c680c281bb4
kelvyn <kelvyn2reis@gmail.com>
2021-03-03 16:25:27 -0300

    README.md

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -38,7 +38,7 @@ export function CountdownProvider({ children}: CountdownProviderProps){
         clearTimeout(countdownTimeout);
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.1 * 60);
+        setTime(25 * 60);
     }
 
     useEffect(()=>{
